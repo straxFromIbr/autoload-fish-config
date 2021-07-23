b2002fc9f15012e46a76c732048cc50e34e642f8
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-07-23 18:52:38 +0900

    2021年 7月23日 金曜日 18時52分38秒 JST

---
+++ conf.d/_reload_configs.fish
@@ -0,0 +1,22 @@
+
+function _reload_myconf -e fish_prompt
+    if status is-interactive
+       set new_sha1sum ( cat $_fish_myconf_dir/.git/refs/heads/main )
+       if test -z $_myconf_sha1sum
+           echo 'config update'
+           fisher update $_fish_myconf_dir > /dev/null &
+           set -U _myconf_sha1sum $new_sha1sum
+       else
+           if test $_myconf_sha1sum != $new_sha1sum
+               echo 'config update'
+               fisher update $_fish_myconf_dir > /dev/null &
+              set -U _myconf_sha1sum $new_sha1sum
+           end
+       end
+   end
+end
+
+
+
+
+ 
